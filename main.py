0c4f3c2731208e7d889ccf1584a8a588afbde28c
usk39 <60925409+usk39@users.noreply.github.com>
2020-02-24 21:05:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -22,30 +22,32 @@ YOUR_CHANNEL_SECRET = os.environ["70d511301ad3f520a15e4889f11b1d52"]
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
+@app.route("/callback", methods=['POST'])
 def callback():
-   # get X-Line-Signature header value
-   signature = request.headers['X-Line-Signature']
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
-   # get request body as text
-   body = request.get_data(as_text=True)
-   app.logger.info("Request body: " + body)
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-   # handle webhook body
-   try:
-       handler.handle(body, signature)
-   except InvalidSignatureError:
-       abort(400)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
-   return 'OK'
+    return 'OK'
+
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-   push_text = event.message.text
-   msg = core.hotpepper(push_text)
-   line_bot_api.reply_message(event.reply_token,TextSendMessage(text=msg))
-   
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
 
 if __name__ == "__main__":
 #    app.run()
-   port = int(os.getenv("PORT"))
-   app.run(host="0.0.0.0", port=port)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
