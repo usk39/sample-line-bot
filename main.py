2fb8c0d12c0a5bd558b2d673ee0d42a5eecdb386
usk39 <60925409+usk39@users.noreply.github.com>
2020-02-24 19:20:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -16,17 +16,12 @@ from linebot.models import (
 app = Flask(__name__)
 
 # 環境変数取得
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["NimJ0uKUdDFSYl3vUuIokWIGMzE+ZPRWecOmKEoaY8pPmRIC0JRboFOWjClMKBh7+fmwmM58whPuvQHHvQqZ2VGUMPVMruihBpLajVyBax1+/m/CGvT8QqUcFJNM9bywGBaMo1qd2fCQIg5SVXJhoAdB04t89/1O/w1cDnyilFU="]
-YOUR_CHANNEL_SECRET = os.environ["70d511301ad3f520a15e4889f11b1d52"]
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
-@app.route("/")
-def hello_world():
-   return "hello world!"
-
-@app.route("/callback", methods=['POST'])
 def callback():
    # get X-Line-Signature header value
    signature = request.headers['X-Line-Signature']
