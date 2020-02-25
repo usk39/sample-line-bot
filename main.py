7438cb2a64c4d42ce3beb14f973b1d74e5b911c6
usk39 <60925409+usk39@users.noreply.github.com>
2020-02-25 22:42:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,47 @@
+# インポートするライブラリ
 from flask import Flask, request, abort
-import os
 
-import core # core.py
+import core
 
 from linebot import (
-   LineBotApi, WebhookHandler
+    LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-   InvalidSignatureError
+    InvalidSignatureError
 )
 from linebot.models import (
-   MessageEvent, TextMessage, TextSendMessage,
+    FollowEvent, MessageEvent, TextMessage, TextSendMessage, ImageMessage, ImageSendMessage, TemplateSendMessage, ButtonsTemplate, PostbackTemplateAction, MessageTemplateAction, URITemplateAction
 )
+import os
 
+# 軽量なウェブアプリケーションフレームワーク:Flask
 app = Flask(__name__)
 
-# 環境変数取得
+
+#環境変数からLINE Access Tokenを設定
 LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+#環境変数からLINE Channel Secretを設定
 LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-@app.route("/")
-def hello_world():
-   return "hello world!"
-
 @app.route("/callback", methods=['POST'])
 def callback():
-   # get X-Line-Signature header value
-   signature = request.headers['X-Line-Signature']
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
-   # get request body as text
-   body = request.get_data(as_text=True)
-   app.logger.info("Request body: " + body)
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-   # handle webhook body
-   try:
-       handler.handle(body, signature)
-   except InvalidSignatureError:
-       abort(400)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
-   return 'OK'
+    return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -49,7 +49,6 @@ def handle_message(event):
    msg = core.hotpepper(push_text)
    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=msg))
    
-
 if __name__ == "__main__":
 #    app.run()
    port = int(os.getenv("PORT"))
